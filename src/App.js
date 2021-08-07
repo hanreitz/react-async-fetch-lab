d670050003599ba7449499900629f6b23cf642bd
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-07 15:20:44 -0400

    commit tree 2cb6ed8b135d9d32ec7174c91fabfe9fad7d4c9e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export class App extends Component {
+  
+  state = {
+    peopleInSpace: []
+  }
+  
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        peopleInSpace: data.people
+      }))
+  }
+
+  makeLi = () => {
+    return this.state.peopleInSpace.map(person => {
+      return <li key={person.name}>{person.name}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div>People Currently in Space:
+        <ul>
+          {this.makeLi()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
